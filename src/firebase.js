c04f3ebab77b171095cb6f005e65b9b5a7b88e9f
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2021-09-25 13:24:30 +0100

    tawa tawa

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRQiZhfp54NiMF2COHIHX68fLPsUNm9Dc",
+  authDomain: "clone-9233f.firebaseapp.com",
+  projectId: "clone-9233f",
+  storageBucket: "clone-9233f.appspot.com",
+  messagingSenderId: "698724377635",
+  appId: "1:698724377635:web:cb10154a796f083bef3d96",
+  measurementId: "G-0QN23ZL6DL"
+};
